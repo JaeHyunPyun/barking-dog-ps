30326c088658289e734724d1588be36fa4275c79
jaehyunpyun <pyunjaehyun@gmail.com>
2026-02-17 16:26:58 +0900

    fix: solve 2752 with algorithm stl

--- 0x02/solved/2752.cpp
+++ 0x02/solved/2752.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <iostream>
 
 using namespace std;
@@ -6,25 +7,21 @@ int main() {
   ios::sync_with_stdio(0);
   cin.tie(0);
 
-  int numArray[3];
+  int num1 = 0;
+  int num2 = 0;
+  int num3 = 0;
 
-  for (int i = 0; i < 3; i++) {
-    cin >> numArray[i];
-  }
+  int num4 = 0;
+  int num5 = 0;
+  int num6 = 0;
 
-  for (int i = 0; i < 2; i++) {
-    for (int j = i + 1; j < 3; j++) {
-      if (numArray[i] > numArray[j]) {
-        int temp = numArray[i];
-        numArray[i] = numArray[j];
-        numArray[j] = temp;
-      }
-    }
-  }
+  cin >> num1 >> num2 >> num3;
 
-  for (int i = 0; i < 3; i++) {
-    cout << numArray[i] << " ";
-  }
+  num4 = min({num1, num2, num3});
+  num6 = max({num1, num2, num3});
+  num5 = num1 + num2 + num3 - num4 - num6;
+
+  cout << num4 << " " << num5 << " " << num6;
 
   return 0;
 }
