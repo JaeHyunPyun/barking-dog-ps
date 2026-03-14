8846c369bfbc219467eba50f7aed8991a5484c14
jaehyunpyun <pyunjaehyun@gmail.com>
2026-03-15 00:35:47 +0900

    feat: solved 18258 using stl

--- 0x06/solved/18258.cpp
+++ 0x06/solved/18258.cpp
@@ -1,45 +1,10 @@
 #include <iostream>
+#include <queue>
 #include <string>
 
 using namespace std;
 
-const int MX = 2000005;
-int queue[MX];
-int head = 0;
-int tail = 0;
-
-int size() { return tail - head; }
-
-int empty() {
-  if (size() <= 0) {
-    return 1;
-  }
-
-  return 0;
-}
-
-void push(int x) { queue[tail++] = x; }
-
-int pop() {
-  if (empty()) {
-    return -1;
-  }
-  return queue[head++];
-}
-
-int front() {
-  if (empty()) {
-    return -1;
-  }
-  return queue[head];
-}
-
-int back() {
-  if (empty()) {
-    return -1;
-  }
-  return queue[tail - 1];
-}
+queue<int> q;
 
 int main() {
   ios::sync_with_stdio(0);
@@ -55,17 +20,30 @@ int main() {
     if (s.compare("push") == 0) {
       int n;
       cin >> n;
-      push(n);
+      q.push(n);
     } else if (s.compare("front") == 0) {
-      cout << front() << "\n";
+      if (q.empty()) {
+        cout << -1 << "\n";
+      } else {
+        cout << q.front() << "\n";
+      }
     } else if (s.compare("back") == 0) {
-      cout << back() << "\n";
+      if (q.empty()) {
+        cout << -1 << "\n";
+      } else {
+        cout << q.back() << "\n";
+      }
     } else if (s.compare("size") == 0) {
-      cout << size() << "\n";
+      cout << q.size() << "\n";
     } else if (s.compare("empty") == 0) {
-      cout << empty() << "\n";
+      cout << q.empty() << "\n";
     } else if (s.compare("pop") == 0) {
-      cout << pop() << "\n";
+      if (q.empty()) {
+        cout << -1 << "\n";
+      } else {
+        cout << q.front() << "\n";
+        q.pop();
+      }
     }
   }
 
